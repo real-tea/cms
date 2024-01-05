8603e3b45f178e2d4d8e4ebc056cec6bde8bf1cb
BSF <bsf@Akashs-Macbook-Air.local>
2024-01-05 14:15:08 +0530

    processing data into db

--- admin/process.php
+++ admin/process.php
@@ -1,35 +1,61 @@
 <?php
+// Check if the "create" button is pressed in the HTML form
 if (isset($_POST["create"])) {
+    // Include the connection file to establish a connection to the database
     include("../connect.php");
+
+    // Get and sanitize data from the form
     $title = mysqli_real_escape_string($conn, $_POST["title"]);
     $summary = mysqli_real_escape_string($conn, $_POST["summary"]);
     $content = mysqli_real_escape_string($conn, $_POST["content"]);
     $date = mysqli_real_escape_string($conn, $_POST["date"]);
-    $sqlInsert = "INSERT INTO posts(date,title, summary, content) VALUES ('$date', '$title', '$summary','$content' )";
-    if(mysqli_query($conn, $sqlInsert)){
+
+    // Create SQL query to insert data into the "posts" table
+    $sqlInsert = "INSERT INTO posts(date, title, summary, content) VALUES ('$date', '$title', '$summary','$content' )";
+
+    // Execute the SQL query and check if it was successful
+    if (mysqli_query($conn, $sqlInsert)) {
+        // Start a session and set a success message
         session_start();
         $_SESSION["create"] = "Post added successfully";
+
+        // Redirect to the index.php page
         header("Location:index.php");
-    }else{
+        echo "data inserted";
+    } else {
+        // If the query fails, display an error message and terminate the script
         die("Data is not inserted!");
+        echo"Data not inserted";
     }
 }
 ?>
 
 <?php
+// Check if the "update" button is pressed in the HTML form
 if (isset($_POST["update"])) {
+    // Include the connection file to establish a connection to the database
     include("../connect.php");
+
+    // Get and sanitize data from the form
     $title = mysqli_real_escape_string($conn, $_POST["title"]);
     $summary = mysqli_real_escape_string($conn, $_POST["summary"]);
     $content = mysqli_real_escape_string($conn, $_POST["content"]);
     $date = mysqli_real_escape_string($conn, $_POST["date"]);
     $id = mysqli_real_escape_string($conn, $_POST["id"]);
+
+    // Create SQL query to update data in the "posts" table
     $sqlUpdate = "UPDATE posts SET title = '$title', summary = '$summary', content = '$content', date = '$date' WHERE id = $id";
-    if(mysqli_query($conn, $sqlUpdate)){
+
+    // Execute the SQL query and check if it was successful
+    if (mysqli_query($conn, $sqlUpdate)) {
+        // Start a session and set a success message
         session_start();
-        $_SESSION["update"] = "Post udpated successfully";
+        $_SESSION["update"] = "Post updated successfully";
+
+        // Redirect to the index.php page
         header("Location:index.php");
-    }else{
+    } else {
+        // If the query fails, display an error message and terminate the script
         die("Data is not updated!");
     }
 }
--- connect.php
+++ connect.php
@@ -1,13 +1,19 @@
 <?php
-    $db_name="cms";
+
+    //to check connection errors if any
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+
+
+    $db_name="Blogger";
 
     $db_pass="";
     $db_user="root";
     $db_host="localhost";
 
-    $connect =  mysqli_connect($db_host , $db_user , $db_pass , $db_name);
+    $conn =  mysqli_connect($db_host , $db_user , $db_pass , $db_name);
 
-    if(!$connect){
+    if(!$conn){
         die("Database not connected");
         echo "error!";
     }
